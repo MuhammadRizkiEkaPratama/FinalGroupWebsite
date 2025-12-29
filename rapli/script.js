f5490caf1899bf0fed208051bf8c670351582d38
raapliest <23523234@students.uii.ac.id>
2025-12-29 21:28:45 +0800

    Adding some details in my own website

--- rapli/script.js
+++ rapli/script.js
@@ -15,3 +15,20 @@ document.querySelectorAll('.project-card, .timeline-item').forEach(el => {
     el.style.transition = 'all 0.6s ease-out';
     observer.observe(el);
 });
+
+// Making a transition to navbar when scroll
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("shrink");
+    } else {
+        navbar.classList.remove("shrink");
+    }
+});
+
+// Adding a hamburger icon in responsive
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+hamburger.addEventListener("click", () =>{
+    navMenu.classList.toggle("active");
+});
